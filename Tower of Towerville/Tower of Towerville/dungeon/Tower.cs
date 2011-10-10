2909704698971efd8b87002713f0c637dedf80aa
Daniel Fairley <poemdexter@gmail.com>
2011-10-09 21:40:48 -0500

    added floor importing and drawing

--- Tower of Towerville/Tower of Towerville/dungeon/Tower.cs
+++ Tower of Towerville/Tower of Towerville/dungeon/Tower.cs
@@ -3,14 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Tower_of_Towerville.framework;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace Tower_of_Towerville.dungeon
 {
     class Tower
     {
-        public int[][] floor { get; set; }
-        public List<Entity> torchlist;
+        public int[][] floorplan { get; set; }
+        //public List<Entity> torchlist;
+        private ContentManager Content;
+        List<string> planLines = new List<string>();
 
+        public Tower(ContentManager content) 
+        {
+            Content = content;
+            getFloorPlanFromFile();
+        }
 
+        public void getFloorPlanFromFile()
+        {
+            initFloorArray();
+
+            try
+            {
+                Stream stream = TitleContainer.OpenStream("dungeon/floor.dat");
+                StreamReader sreader = new StreamReader(stream);
+                int lineNum = 0;
+                while (sreader.Peek() >= 0)
+                {
+                    fillFloorRow(sreader.ReadLine(), lineNum);
+                    lineNum++;
+                }
+                stream.Close();
+            }
+            catch (System.IO.FileNotFoundException) {}
+        }
+
+        private void fillFloorRow(string line, int num)
+        {
+            for (int a = 0; a < line.Length; a++)
+                floorplan[a][num] = (int)Char.GetNumericValue(line[a]);
+        }
+
+        private void initFloorArray()
+        {
+            floorplan = new int[17][];
+            for (int a = 0; a < floorplan.Length; a++)
+            {
+                floorplan[a] = new int[17];
+            }
+
+            for (int x = 0; x < floorplan.Length; x++)
+            {
+                for (int y = 0; y < floorplan[x].Length; y++)
+                {
+                    floorplan[x][y] = 0;
+                }
+            }
+        }
     }
 }
--- Tower of Towerville/Tower of Towerville/screens/screens/MainGameScreen.cs
+++ Tower of Towerville/Tower of Towerville/screens/screens/MainGameScreen.cs
@@ -6,27 +6,38 @@ using Tower_of_Towerville.screens.framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Tower_of_Towerville.dungeon;
 
 namespace Tower_of_Towerville.screens.screens
 {
+    enum EnvTiles
+    {
+        WallExp = 1,
+        WallReg = 2,
+        Floor = 3
+    }
+
     class MainGameScreen : GameScreen
     {
         Dictionary<string, Texture2D> spriteDict;
+        Tower tower;
+        private const int XtoCenter = 88;
 
         public MainGameScreen()
         {
- 
+
         }
 
         public override void LoadContent()
         {
             loadSpriteDictionary();
+            tower = new Tower(screenManager.Game.Content);
         }
 
         public override void HandleInput(InputState input)
         {
             if (input.IsNewKeyPress(Keys.NumPad0))
-            {}
+            { }
         }
 
         public override void Update(GameTime gameTime)
@@ -43,12 +54,26 @@ namespace Tower_of_Towerville.screens.screens
             spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, null);
 
             // draw things
-            Texture2D texture = spriteDict["floor"];
-            for (int x = 0; x < 17; x++)
+            Texture2D texture;
+            for (int x = 0; x < tower.floorplan.Length; x++)
             {
-                for (int y = 0; y < 17; y++)
+                for (int y = 0; y < tower.floorplan[0].Length; y++)
                 {
-                    spriteBatch.Draw(texture, new Vector2(x * (4f * texture.Width), y * (4f * texture.Height)), null, Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 0f);
+                    switch (tower.floorplan[x][y])
+                    {
+                        case (int)EnvTiles.WallExp:
+                            texture = spriteDict["wall_exp"];
+                            spriteBatch.Draw(texture, new Vector2(x * (4f * texture.Width) + XtoCenter, y * (4f * texture.Height)), null, Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 0f);
+                            break;
+                        case (int)EnvTiles.WallReg:
+                            texture = spriteDict["wall_reg"];
+                            spriteBatch.Draw(texture, new Vector2(x * (4f * texture.Width) + XtoCenter, y * (4f * texture.Height)), null, Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 0f);
+                            break;
+                        case (int)EnvTiles.Floor:
+                            texture = spriteDict["floor"];
+                            spriteBatch.Draw(texture, new Vector2(x * (4f * texture.Width) + XtoCenter, y * (4f * texture.Height)), null, Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 0f);
+                            break;
+                    }
                 }
             }
             spriteBatch.End();
@@ -60,6 +85,8 @@ namespace Tower_of_Towerville.screens.screens
 
             spriteDict.Add("wizard", screenManager.Game.Content.Load<Texture2D>("character/wizard"));
             spriteDict.Add("floor", screenManager.Game.Content.Load<Texture2D>("environment/floor"));
+            spriteDict.Add("wall_exp", screenManager.Game.Content.Load<Texture2D>("environment/wall_exp"));
+            spriteDict.Add("wall_reg", screenManager.Game.Content.Load<Texture2D>("environment/wall_reg"));
         }
     }
 }
